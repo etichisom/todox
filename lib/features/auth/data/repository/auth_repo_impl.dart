6602b712c73c33ca74461dde6b1996e77c1d6573
Chisom Eti <chisom@connou.app>
2023-06-05 12:47:43 +0100

    add:added app icons and launcher screen

--- lib/features/auth/data/repository/auth_repo_impl.dart
+++ lib/features/auth/data/repository/auth_repo_impl.dart
@@ -8,7 +8,9 @@ class AuthRepositoryImpl extends AuthRepository {
   Future<UserCredential?> signInWithGoogle() async {
     try {
       // Trigger the authentication flow
-      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAccount? googleUser = await GoogleSignIn(
+        forceCodeForRefreshToken: true
+      ).signIn();
 
       // Obtain the auth details from the request
       final GoogleSignInAuthentication? googleAuth =
